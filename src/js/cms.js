e7831ed0716056ea65dce91b29337b159a7649fe
apt-get2Update <thirumurugan3131@gmail.com>
2021-01-13 18:43:27 +0530

    about page dispaly issue fixed

--- src/js/cms.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import CMS from "netlify-cms-app";
-
-// Import main site styles as a string to inject into the CMS preview pane
-import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../css/main.css";
-CMS.init();
